25da73d5bc8eba02157f1eaf34899157646e6433
Edwin Felipe <edwinisfelipe25@gmail.com>
2020-02-29 09:16:51 -0400

    initial board disposition setted

--- js/main.ts
+++ js/main.ts
@@ -3,6 +3,57 @@ const cx: CanvasRenderingContext2D = canvas.getContext('2d');
 
 
 class Board {
+
+    initialDisposition: Piece[][] = [
+        [
+            new Piece('rook', 'black', new Position(0, 0)),
+            new Piece('knight', 'black', new Position(0, 0)),
+            new Piece('bishop', 'black', new Position(0, 0)),
+            new Piece('queen', 'black', new Position(0, 0)),
+            new Piece('king', 'black', new Position(0, 0)),
+            new Piece('bishop', 'black', new Position(0, 0)),
+            new Piece('knight', 'black', new Position(0, 0)),
+            new Piece('rook', 'black', new Position(0, 0))
+        ],
+        [
+            new Piece('pawn', 'black', new Position(0, 0)),
+            new Piece('pawn', 'black', new Position(0, 0)),
+            new Piece('pawn', 'black', new Position(0, 0)),
+            new Piece('pawn', 'black', new Position(0, 0)),
+            new Piece('pawn', 'black', new Position(0, 0)),
+            new Piece('pawn', 'black', new Position(0, 0)),
+            new Piece('pawn', 'black', new Position(0, 0)),
+            new Piece('pawn', 'black', new Position(0, 0))
+        ],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [
+            new Piece('pawn', 'white', new Position(0, 0)),
+            new Piece('pawn', 'white', new Position(0, 0)),
+            new Piece('pawn', 'white', new Position(0, 0)),
+            new Piece('pawn', 'white', new Position(0, 0)),
+            new Piece('pawn', 'white', new Position(0, 0)),
+            new Piece('pawn', 'white', new Position(0, 0)),
+            new Piece('pawn', 'white', new Position(0, 0)),
+            new Piece('pawn', 'white', new Position(0, 0))
+        ],
+        [
+            new Piece('rook', 'white', new Position(0, 0)),
+            new Piece('knight', 'white', new Position(0, 0)),
+            new Piece('bishop', 'white', new Position(0, 0)),
+            new Piece('queen', 'white', new Position(0, 0)),
+            new Piece('king', 'white', new Position(0, 0)),
+            new Piece('bishop', 'white', new Position(0, 0)),
+            new Piece('knight', 'white', new Position(0, 0)),
+            new Piece('rook', 'white', new Position(0, 0))
+        ],
+    ];
+
+
+
+
     drawBoard() {
         const blockSize = canvas.width / 8;
         for (var row = 0; row < 8; row++) {
@@ -14,42 +65,60 @@ class Board {
             }
         }
     }
+
+
+    initPieces() {
+        const blockSize = canvas.width / 8;
+        this.initialDisposition.map((row, rowIndex) => row.map(function (cell, cellIndex) {
+            if (cell == null) {
+
+            } else {
+                cell.position = new Position(cellIndex * blockSize, rowIndex * blockSize);
+                cell.draw();
+            }
+        }));
+    }
 }
 
 class Piece {
     type;
     team;
-    assets;
-    constructor(type: 'bishop' | 'king' | 'queen' | 'knight' | 'pawn' | 'rook', team: 'white' | 'black') {
+    position: Position
+    assets: string;
+    constructor(type: 'bishop' | 'king' | 'queen' | 'knight' | 'pawn' | 'rook', team: 'white' | 'black', position: Position) {
         this.type = type;
         this.team = team;
-        this.assets = {
-            'bishop': `../assets/bishop${this.team == 'white' ? 'W' : 'B'}2.png`,
-            'king': `../assets/king${this.team == 'white' ? 'W' : 'B'}2.png`,
-            'queen': `../assets/queen${this.team == 'white' ? 'W' : 'B'}2.png`,
-            'pawn': `../assets/pawn${this.team == 'white' ? 'W' : 'B'}2.png`,
-            'rook': `../assets/rook${this.team == 'white' ? 'W' : 'B'}2.png`,
-            'knight': `../assets/knight${this.team == 'white' ? 'W' : 'B'}2.png`
-        };
+        this.position = position;
+        this.assets = `../assets/${this.type}${this.team == 'white' ? 'W' : 'B'}2.png`;
+
     }
 
 
     draw() {
         var image = new Image();
 
-        image.onload = function () {
-            cx.drawImage(image, 40, 40);
-        }
+        image.onload = () => cx.drawImage(image, this.position.x, this.position.y);
 
-        image.src = this.assets[this.type];
+
+        image.src = this.assets;
     }
 }
 
+class Position {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
 function init() {
     const board = new Board();
-    const piece = new Piece('pawn', 'black');
     board.drawBoard();
-    piece.draw();
+    board.initPieces();
 }
 
 init();
